cc5f9b55f4cf6c8d45f72db45718e8738f9c3085
Charison <charison.gillbranion@gmail.com>
2023-03-05 13:27:12 -0800

    Add input loop

--- basic_MOO.py
+++ basic_MOO.py
@@ -1,7 +1,26 @@
 from MOO import MOO
 
 def main():
-	print(__name__)
+	# FIXME: Read from cl
+	basic_moo = MOO(4, 6, 8)
+	basic_moo.set_code(4535)
+	code_correct = False
+	panel = {'D/WRF':0, 'LF/FS':0}
+	
+	while code_correct is not None:
+		try:
+			moo_code = int(input("Enter the MOO code:"))
+			code_correct = basic_moo.try_code(moo_code)
+			if not code_correct:
+				print("ACCESS DENIED")
+			else:
+				print("ACCESS GRANTED")
+				break
+			print(f"{basic_moo.tries_history}")
+		except ValueError:
+			continue
+		except Exception as ex:
+			print(ex)
 
 if __name__ == '__main__':
 	main()
