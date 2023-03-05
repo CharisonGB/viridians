703b8ec59efd478e94d6cce63720a5ed772d5039
Charison <charison.gillbranion@gmail.com>
2023-03-05 11:50:50 -0800

    Change to setting correct code outside of __init__

--- MOO.py
+++ MOO.py
@@ -1,14 +1,32 @@
 class MOO():
-	def __init__(self, dwrf: int, dial_size: int, max_attempts: int):
-		self.dwrf_key = [int(d) for d in str(dwrf)]
-		self.lffs_key = [0]*(dial_size + 1)
-		for d in self.dwrf_key: self.lffs_key[d] = 1
-		
-		self.dial_count = len(self.dwrf_key)
+	def __init__(self, dial_count: int, dial_size: int, max_attempts: int):
+		self.dial_count = dial_count
 		self.dial_size = dial_size
 		
 		self.max_attempts = max_attempts
 		self.attempt_history = []
+		
+		self.code_min = int(str(1)*dial_count)
+		self.code_max = int(str(dial_size)*dial_count)
+		self.dwrf_key = [0]*dial_count
+		self.lffs_key = [0]*(dial_size + 1)
+	
+	def set_code(self, moo_code: int):
+		try:
+			if self.code_max < moo_code or moo_code < self.code_min:
+				raise ValueError(f"Code has exactly {self.dial_count} digits.")
+			
+			self.dwrf_key = [int(d) for d in str(moo_code)]
+			
+			if max(self.dwrf_key) > self.dial_size or min(self.dwrf_key) < 1:
+				raise ValueError(f"Dial out of range {1}..{self.dial_size}.")
+			
+			for d in self.dwrf_key:
+				self.lffs_key[d] = 1
+		
+		except ValueError as ve:
+			print(f"Failed to set MOO code to {moo_code}: {ve}")
+	
 	
 	def calc_remaining_attempts(self):
 		return self.max_attempts - len(self.attempt_history)
@@ -38,10 +56,11 @@ class MOO():
 		return dwrf, lffs
 
 def main():
-	moo = MOO(4535, 6, 8)
-	answer = 3456
-	moo.attempt(answer)
-	print(moo.attempt_history)
+	moo = MOO(4, 6, 8)
+	answer = 1349
+	moo.set_code(answer)
+	#moo.attempt(answer)
+	#print(moo.attempt_history)
 	
 if __name__ == '__main__':
 	main()
