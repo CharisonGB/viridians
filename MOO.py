16f3f360ec097b294c5b4a0f2c564dd63d3f7ec9
Charison <charison.gillbranion@gmail.com>
2023-03-05 01:06:29 -0800

    Add attempt history

--- MOO.py
+++ MOO.py
@@ -1,10 +1,17 @@
 class MOO():
-	def __init__(self, dwrf: int, dial_size: int):
+	def __init__(self, dwrf: int, dial_size: int, max_attempts: int):
 		self.dwrf_key = [int(d) for d in str(dwrf)]
-		self.dial_count = len(self.dwrf_key)
-		self.dial_size = dial_size
 		self.lffs_key = [0]*(dial_size + 1)
 		for d in self.dwrf_key: self.lffs_key[d] = 1
+		
+		self.dial_count = len(self.dwrf_key)
+		self.dial_size = dial_size
+		
+		self.max_attempts = max_attempts
+		self.attempt_history = []
+	
+	def calc_remaining_attempts(self):
+		return self.max_attempts - len(self.attempt_history)
 	
 	def calc_dwrf(self, dial_values: [int]):
 		dwrf = 0
@@ -25,14 +32,16 @@ class MOO():
 		dial_values = [int(a) for a in str(answer)]
 		dwrf = self.calc_dwrf(dial_values)
 		lffs = self.calc_lffs(dial_values)
-		
-		print(f"D/WRF: {dwrf} | LF/FS: {lffs}")
-		return True
+		self.attempt_history.append(f"TRY:{answer} | D/WRF:{dwrf} | LF/FS:{lffs}")
+		if self.max_attempts != 0 and self.calc_remaining_attempts() <= 0:
+			return None
+		return dwrf, lffs
 
 def main():
-	moo = MOO(4535, 6)
+	moo = MOO(4535, 6, 8)
 	answer = 3456
 	moo.attempt(answer)
+	print(moo.attempt_history)
 	
 if __name__ == '__main__':
 	main()
